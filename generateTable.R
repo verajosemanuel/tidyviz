84bb35d2e1457ef756360bb6c02f0e6501d375c4
Jose Manuel <vera.josemanuel@gmail.com>
2017-07-27 09:22:23 +0200

    sessioninfo charlatan pkggraph webglobe kandinsky

--- generateTable.R
+++ generateTable.R
@@ -62,7 +62,8 @@ df.git <-
 	    "Stan125/GREA",
 		"tarakc02/rmapzen",
 		"lchiffon/wordcloud2",
-		"bhaskarvk/leaflet.extras"
+		"bhaskarvk/leaflet.extras",
+		"gsimchoni/kandinsky"
     ),
     "Title" = c(
       "colformat provides tools for styling columns of data, artfully using colour and unicode characters.",
@@ -108,7 +109,8 @@ df.git <-
 	    "Gotta Read Em All: RStudio Add-In to interactively read ALL the data into R using RIO.",
 		"R client for the Mapzen API",
 		"R interface to wordcloud for data visualization.",
-		"Extra functionality for leaflet R package."
+		"Extra functionality for leaflet R package.",
+		"Turn any dataset into a Kandinsky painting"
     ),
     "Description" = c(
       "colformat is not designed for end-users but will eventually be incorporated in packages like tibble",
@@ -158,7 +160,8 @@ df.git <-
 	    "GREA (Gotta Read 'Em All) is an RStudio Add-In assisting to read all popular file formats into R through R base functions and rio. In the beginning, the user selects a file on his/her computer. After some optional adjustments (which are done interactively), the proper function to read the file is pasted into the console, with an object name that can be specified by the user. Supported file formats include Stata (.dta), SPSS (.sav), Matlab (.mat), Excel (.xls, .xlsx) and various text/delimited-formats (.raw, .csv, .txt, .asc, .dat, etc.).",
 		"rmapzen is a client for the Mapzen API. For an introduction, detailed examples, and installation instructions, see: https://tarakc02.github.io/rmapzen/. For more information about the Mapzen API, see https://mapzen.com/documentation/.",
 		"This package provides an HTML5 interface to wordcloud for data visualization. Timdream’s wordcloud2.js is used in this package.",
-		"The goal of leaflet.extras package is to provide extra functionality to the leaflet R package using various leaflet plugins."
+		"The goal of leaflet.extras package is to provide extra functionality to the leaflet R package using various leaflet plugins.",
+		"To generate random Wassily Kandinsky paintings or even make any dataset into one."
 		),
     "URL" = c(
       "https://github.com/hadley/colformat",
@@ -204,7 +207,8 @@ df.git <-
 	    "https://github.com/Stan125/GREA",
 		"https://github.com/tarakc02/rmapzen",
 		"https://cran.r-project.org/web/packages/wordcloud2/vignettes/wordcloud.html",
-		"http://r-spatial.org/r/2017/01/30/mapedit_intro.html"
+		"http://r-spatial.org/r/2017/01/30/mapedit_intro.html",
+		"http://giorasimchoni.com/2017/07/30/2017-07-30-data-paintings-the-kandinsky-package/"
     )
   )
 
