84eb0d04f269c8d7a975ace9b5feb1c550d50b43
Jose Manuel <vera.josemanuel@gmail.com>
2017-11-06 12:10:36 +0100

    first complete separated recipes

--- Dockerfile
+++ Dockerfile
@@ -1,49 +1,28 @@
-FROM jvera/tidyviz-wrangler:latest
+FROM jvera/tidyviz-gis:latest
 
 LABEL maintainer "vera.josemanuel@gmail.com"
 
 ADD github_installs.R /tmp/github_installs.R
 
-RUN install2.r --error arules \
-arulesViz \
-bayesAB \
-bayesboot \
-bigrquery \
+RUN install2.r --error bigrquery \
 bookdown \
-brms \
-broom \
-BTYD \
-BTYDplus \
 cartography \
-changepoint \
 choroplethr \
 choroplethrMaps \
-confinterpret \
-cshapes \
 DiagrammeR \
 d3heatmap \
 d3Tree \
 dygraphs \
-e1071 \
-effects \
 expss \
-FFTrees \
-forecast \
-forecTheta \
-gap \
-gapminder \
 gdalUtils \
 geonames \
 geoR \
 geosphere \
 GeoXp \
-glmnet \
 heatmaply \
 hexSticker \
 liftr \
-lime \
-linemap \
-liquidSVM \
+manipulate \
 mapdata \
 mapedit \
 mapmisc \
@@ -51,58 +30,35 @@ maps \
 maptools \
 mapview \
 metricsgraphics \
-Modeler \
 mschart \
 networkD3 \
-party \
-PerformanceAnalytics \
 pipefittr \
 placement \
 plotKML \
 postGIStools \
+printr \
 quantmod \
-randomForest \
 raster \
 rasterVis \
-robets \
 rgdal \
 rgl \
 rgeos \
 RgoogleMaps \
 rmapzen \
-rms \
-rpart \
-rpart.plot \
-rpg \
 rpostgis \
-rsample \
 rts \
 rvg \
 rworldmap \
-sas7bdat \
-scanstatistics \
 shapefiles \
-shiny \
-shinystan \
 spatstat \
-leaflet.minicharts \
 reprex \
 smpic \
 sp \
-stlplus \
-sweep \
 threejs \
-tibbletime \
-tidygraph \
-timetk \
-tidyquant \
 tmap \
 tmaptools \
 tseries \
 visNetwork \
-WordR \
-xts \
-zoo \
 && Rscript /tmp/github_installs.R \
 && apt-get clean \
 && rm -rf /var/lib/apt/lists/ \
--- github_installs.R
+++ github_installs.R
@@ -1,23 +1,10 @@
 library(devtools)
 
 install_github("ColinFay/aside")
-
-# extras
-install_github("dyerlab/popgraph")
-install_github("trestletech/shinyAce")
-install_github("swarm-lab/editR") # requires devtools::shinyAce shiny shinyFiles shinyBS rmarkdown knitr
-install_github("EmilHvitfeldt/ggpage")
 install_github("hrbrmstr/msgxtractr")
-install_github("dgrtwo/gganimate")
 install_github("rstats-db/RPostgres")
-install_github("yihui/printr")
-install_github("hafen/geofacet")
-install_github("hrbrmstr/hrbrthemes")
-install_github("hrbrmstr/waffle")
-install_github("bhaskarvk/leaflet.extras")
 install_github("vqv/ggbiplot")
 install_github("rich-iannone/blastula")
-install_github("wilkox/ggfittext")
 install_github("ropensci/tabulizerjars")
 install_github("ropensci/tabulizer")
 install_github("ThinkRstat/littleboxes")
@@ -27,10 +14,14 @@ install_github("gsimchoni/kandinsky")
 install_github("datacamp/RDocumentation")
 install_github("edgararuiz/dbplot")
 install_github("krlmlr/styler")
+install_github("ThinkR-open/remedy")
 install_github("AntoineGuillot2/D3partitionR")
 install_github("hafen/trelliscopejs")
-install_github("ThinkR-open/remedy")
-# lemon ##ggplot
-# ggridges
-# wordcloud2
-# mapsapi
+install_github("wilkox/ggfittext")
+install_github("hrbrmstr/waffle")
+install_github("bhaskarvk/leaflet.extras")
+install_github("dgrtwo/gganimate")
+install_github("EmilHvitfeldt/ggpage")
+install_github("dyerlab/popgraph")
+install_github("trestletech/shinyAce")
+install_github("swarm-lab/editR") # requires devtools::shinyAce shiny shinyFiles shinyBS rmarkdown knitr
