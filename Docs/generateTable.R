01a3fdfde246c42940908def262f9be807cda02d
Jose <vera.josemanuel@gmail.com>
2018-06-27 15:48:54 +0200

    reportfactory dqrng

--- Docs/generateTable.R
+++ Docs/generateTable.R
@@ -1,6 +1,8 @@
 rm(list = ls())
 
 library(tidyverse)
+library(basictabler)
+library(openxlsx)
 
 cran <- tools::CRAN_package_db()
 
@@ -18,6 +20,19 @@ cran <- cran[, c("Package", "Title", "Description", "URL", "Version","Date")]
 
 df <- dplyr::right_join(cran, mycran)
 
-rio::export(df, file = "D:/kschool/Docker/tidyviz/Docs/cran_packages.xlsx", format = "xlsx" )
+
 write.csv(df, file = "D:/kschool/Docker/tidyviz/Docs/cran_packages.csv", row.names = FALSE, sep = ';')
 
+
+a <- qtbl(df)
+
+wb <- createWorkbook(creator = Sys.getenv("USERNAME"))
+addWorksheet(wb, "Data")
+a$writeToExcelWorksheet(
+  wb = wb,
+  wsName = "Data",
+  topRowNumber = 2,
+  leftMostColumnNumber = 2,
+  applyStyles = TRUE
+)
+saveWorkbook(wb, file = "D:/kschool/Docker/tidyviz/Docs/cran_packages.xlsx", overwrite = TRUE)
